0a8658346b2b642f8e800f7c18053e26bb94b68a
Alex <alexyns28@gmail.com>
2015-08-06 15:19:48 +0800

    added getpaginationlinkblog function on Link CLass

--- override/classes/Link.php
+++ override/classes/Link.php
@@ -657,4 +657,85 @@ class Link extends LinkCore
 		return $url.($p == 1 ? '?p=1' : (!strstr($url, '?') ? '?' : '&').'p='.(int)$p);
 	}
 
+	/**
+	 * Get pagination link
+	 *
+	 * @param string $type Controller name
+	 * @param int $id_object
+	 * @param boolean $nb Show nb element per page attribute
+	 * @param boolean $sort Show sort attribute
+	 * @param boolean $pagination Show page number attribute
+	 * @param boolean $array If false return an url, if true return an array
+	 */
+	public function getPaginationLinkBlog($type, $id_object, $nb = false, $sort = false, $pagination = false, $array = false, $nb_page)
+	{
+		// If no parameter $type, try to get it by using the controller name
+		if (!$type && !$id_object)
+		{
+			$method_name = 'get'.Dispatcher::getInstance()->getController().'Link';
+			if (method_exists($this, $method_name) && isset($_GET['id_'.Dispatcher::getInstance()->getController()]))
+			{
+				$type = Dispatcher::getInstance()->getController();
+				$id_object = $_GET['id_'.$type];
+			}
+		}
+
+		if ($type && $id_object)
+			$url = $this->{'get'.$type.'Link'}($id_object, null);
+		else
+		{
+			if (isset(Context::getContext()->controller->php_self))
+				$name = Context::getContext()->controller->php_self;
+			else
+				$name = Dispatcher::getInstance()->getController();
+			$url = $this->getPageLink($name);
+		}
+
+		$vars = array();
+		$vars_nb = array('n', 'search_query');
+		$vars_sort = array('orderby', 'orderway');
+		$vars_pagination = array('p');
+
+		foreach ($_GET as $k => $value)
+		{
+			if ($k != 'id_'.$type && $k != 'controller')
+			{
+				if (Configuration::get('PS_REWRITING_SETTINGS') && ($k == 'isolang' || $k == 'id_lang'))
+					continue;
+				$if_nb = (!$nb || ($nb && !in_array($k, $vars_nb)));
+				$if_sort = (!$sort || ($sort && !in_array($k, $vars_sort)));
+				$if_pagination = (!$pagination || ($pagination && !in_array($k, $vars_pagination)));
+				if ($if_nb && $if_sort && $if_pagination)
+				{
+					if (!is_array($value))
+						$vars[urlencode($k)] = $value;
+					else
+					{
+						foreach (explode('&', http_build_query(array($k => $value), '', '&')) as $key => $val)
+						{
+							$data = explode('=', $val);
+							$vars[urldecode($data[0])] = $data[1];
+						}
+					}
+				}
+			}
+		}
+
+		if (!$array)
+			if (count($vars))
+				return $url.(($this->allow == 1 || $url == $this->url) ? '?' : '&').http_build_query($vars, '', '&');
+			else
+				return $url;
+		
+		$vars['requestUrl'] = $url;
+
+		if ($type && $id_object)
+			$vars['id_'.$type] = (is_object($id_object) ? (int)$id_object->id : (int)$id_object);
+			
+		if (!$this->allow == 1)
+			$vars['controller'] = Dispatcher::getInstance()->getController();
+
+		// return $vars ;
+	}
+
 }
